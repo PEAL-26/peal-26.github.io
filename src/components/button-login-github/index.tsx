ee12fd693dc647f73d9f28e9a3d92067702e5dff
peal-26 <edilasio@live.com>
2023-06-19 22:36:37 +0100

    update to deploy

--- src/components/button-login-github/index.tsx
+++ src/components/button-login-github/index.tsx
@@ -1,37 +1,37 @@
-import { signInWithPopup, GithubAuthProvider } from 'firebase/auth'
+// import { signInWithPopup, GithubAuthProvider } from 'firebase/auth'
 
-import { auth } from '@/libs/firebase'
+// import { auth } from '@/libs/firebase'
 
 export default function ButtonLoginGithub() {
   const handleLogin = () => {
-    const provider = new GithubAuthProvider()
-    provider.setCustomParameters({
-      allow_signup: 'false',
-    })
+    // const provider = new GithubAuthProvider()
+    // provider.setCustomParameters({
+    //   allow_signup: 'false',
+    // })
 
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        const credential = GithubAuthProvider.credentialFromResult(result)
-        const token = credential?.accessToken
+    // signInWithPopup(auth, provider)
+    //   .then((result) => {
+    //     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    //     const credential = GithubAuthProvider.credentialFromResult(result)
+    //     const token = credential?.accessToken
 
-        // The signed-in user info.
-        const user = result.user
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
+    //     // The signed-in user info.
+    //     const user = result.user
+    //     // IdP data available using getAdditionalUserInfo(result)
+    //     // ...
 
-        console.log(JSON.stringify({ token, result }, null, 5))
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code
-        const errorMessage = error.message
-        // The email of the user's account used.
-        const email = error.customData.email
-        // The AuthCredential type that was used.
-        const credential = GithubAuthProvider.credentialFromError(error)
-        // ...
-      })
+    //     console.log(JSON.stringify({ token, result }, null, 5))
+    //   })
+    //   .catch((error) => {
+    //     // Handle Errors here.
+    //     const errorCode = error.code
+    //     const errorMessage = error.message
+    //     // The email of the user's account used.
+    //     const email = error.customData.email
+    //     // The AuthCredential type that was used.
+    //     const credential = GithubAuthProvider.credentialFromError(error)
+    //     // ...
+    //   })
   }
 
   return (
