0e93b47e226f9a6f22f9e45d63af48b7c5c809e3
peal-26 <edilasio@live.com>
2023-06-30 15:47:44 +0100

    teste17

--- src/config/firebase-config.tsx
+++ src/config/firebase-config.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
@@ -8,3 +10,5 @@ export const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
   keySiteReCAPTCHAEnterprise: process.env.NEXT_PUBLIC_FIREBASE_KEY_SITE_RECAPTCHA_ENTERPRISE ?? '',
 }
+
+console.log(firebaseConfig)
