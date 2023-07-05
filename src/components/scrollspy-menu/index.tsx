2fe91c5cdfa92cdb4fdc8ee8584edb9cec02dfbf
peal-26 <edilasio@live.com>
2023-07-05 09:09:32 +0100

    update scrollspy biografia

--- src/components/scrollspy-menu/index.tsx
+++ src/components/scrollspy-menu/index.tsx
@@ -26,6 +26,7 @@ export default function ScrollspyMenu({ menus, current, sectionRefs, isSticked }
   const [classNameLink, setClassNameLink] = useState<ClassNameLinkProps>({} as ClassNameLinkProps)
   const [containerMenu, setContainerMenu] = useState('')
   const [visibleMenu, setVisibleMenu] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
   function handleAnchorClick(event: MouseEvent, elementIndex: number) {
     event.preventDefault()
@@ -51,7 +52,7 @@ export default function ScrollspyMenu({ menus, current, sectionRefs, isSticked }
 
     const classNameContainer = {
       mobile: 'fixed top-0 left-0 w-full transition-all',
-      other: 'rounded-md min-h-screen sticky top-0 w-full self-start w-[30%] lg:w-[20%]',
+      other: 'rounded-md min-h-screen sticky top-0 w-full self-start w-[20%]',
     }
 
     const classNameHeaderTitle = {
@@ -64,8 +65,18 @@ export default function ScrollspyMenu({ menus, current, sectionRefs, isSticked }
     setVisibleMenu(isMobile ? classNameHeaderTitle[isSticked ? 'show' : 'hide'] : '')
   }, [isMobile, isSticked])
 
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) return null
+
   return (
-    <div className={`bg-black p-3 ${containerMenu} ${visibleMenu} `}>
+    <div
+      className={`${
+        current > -1 ? '' : 'max-sm:hidden'
+      } bg-black p-3 ${containerMenu} ${visibleMenu} `}
+    >
       <ul className="flex flex-col gap-3 text-white">
         {menus.map(({ id, titulo }, index) => (
           <li
