ba7dbdce770ef3ddfa7d3692924153ea3e019a1a
peal-26 <edilasio@live.com>
2023-07-27 09:24:09 +0100

    update head to SEO Google

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <meta name="google-site-verification" content="RzsRHF5u3f59UtDAZDioKmwL0_X6YcY5toSev4w3970" />
+      <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
     </>
   )
 }
