3e560f3d76ae8f4592f42d3ff5c95ff1120340a1
peal-26 <edilasio@live.com>
2023-07-27 10:09:03 +0100

    update to SEO Google

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
   return (
-    <>
+    <head>
       <meta name="google-site-verification" content="RzsRHF5u3f59UtDAZDioKmwL0_X6YcY5toSev4w3970" />
-      <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
-    </>
+      <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
+    </head>
   )
 }
