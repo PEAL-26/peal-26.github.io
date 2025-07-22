ba5110d2842a2a9801ae7613fcca1813e40232b3
Pedro Edilásio Araújo Lopes <8791669+PEAL-26@users.noreply.github.com>
2025-07-22 07:13:03 +0100

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <meta name="google-site-verification" content="RzsRHF5u3f59UtDAZDioKmwL0_X6YcY5toSev4w3970" />
       <meta name="google-adsense-account" content="ca-pub-1818615327749964" />
       <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
+      <meta name="129fba70f8efbb260c608a6bdadb7eaf4498955c" content="129fba70f8efbb260c608a6bdadb7eaf4498955c" />
     </head>
   )
 }
